79f25daeacf84f83175d96ec542b737a11646ff9
yamao-sys <yamaopanku@gmail.com>
2025-01-06 11:02:59 +0900

    不要なspec配下を削除

--- packs/requirements/app/controllers/requirements/requirements_controller.rb
+++ packs/requirements/app/controllers/requirements/requirements_controller.rb
@@ -1,7 +1,7 @@
 module Requirements
   class RequirementsController < ApplicationController
     def index
-      render json: { message: "Hello from RequirementsController Engine!!!" }
+      render json: { message: "Hello from RequirementsController Engine!" }
     end
   end
 end
--- packs/requirements/spec/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Requirements::Engine => "/requirements"
-end
